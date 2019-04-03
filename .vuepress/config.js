20d1f76621c51021c122f89cfdebd9e6f7b3f134
Brian Norris <bdnorris@gmail.com>
2019-04-03 15:28:34 -0500

    Update lectures

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -32,6 +32,8 @@ module.exports = {
           "lectures/lecture15-specificity.md",
           "lectures/lecture16-position-and-display.md",
           "lectures/lecture17-layout-flexbox.md",
+          "lectures/lecture18-responsive-code.md",
+          "lectures/lecture19-grid.md",
         ]
       },
       {
